86b679b2a7c6daa9c4d6a0ead8aeffd37e2b9460
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-10-07 08:44:53 +0300

    init: game settings layout

---
+++ pointing-poker/src/components/Pages/LobbyPage/GameSettings/GameSettings.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './gameSettings.scss';
+
+const GameSettings = () => {
+  return (
+    <div>
+      <h3 className="lobby__title">Game settings:</h3>
+    </div>
+  );
+};
+
+export default GameSettings;
